a32a0abe1fd0ab1e3a20d53761ef36adcad4da94
Maxwell6612 <Maxwell6612@protonmail.ch>
2021-03-30 21:51:22 +0300

    make components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return <nav className='nav'>
+    <div>Profile</div>
+    <div>Messages</div>
+    <div>News</div>
+    <div>Music</div>
+    <div>Settings</div>
+  </nav>
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Profile = () => {
+    return <div className='content'>
+    <div><img src="https://www.ephotozine.com/articles/how-to-use-horizontal-lines-successfully-in-your-images-17104/images/P9020034_dxo.jpg" alt=''/></div>
+    <div>
+      ava+desc
+    </div>
+    <div>
+      My post
+        <div> New post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+        <div>post 3</div>
+    </div>
+  </div>
+}
+
+export default Profile;
