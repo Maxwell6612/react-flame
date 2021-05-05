4a9545e6c5b8e86dfe2a65fd2f30337db351af4d
Maxwell6612 <Maxwell6612@protonmail.ch>
2021-05-05 20:55:27 +0300

    add dialog props

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import MyPosts from './MyPosts/Post/MyPosts';
 import s from './Profile.module.css';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
-    return <div>
-    <div><img src="https://www.ephotozine.com/articles/how-to-use-horizontal-lines-successfully-in-your-images-17104/images/P9020034_dxo.jpg" alt=''/></div>
-    <div>
-      ava+desc
-    </div>
-    <MyPosts />
-  </div>
+    return (
+      <div>
+          <ProfileInfo/>
+          <MyPosts />
+      </div>
+    )
 }
 
 export default Profile;
