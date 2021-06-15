ac48e3d4328f05c250de631eacdfd29d6633a267
Maxwell6612 <Maxwell6612@protonmail.ch>
2021-06-15 21:32:25 +0300

    copy state in ProfileReducer

--- src/index.js
+++ src/index.js
@@ -5,22 +5,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 
-let rerenderEntireTree = (state) => {
-  ReactDOM.render(
-    <BrowserRouter>
+ReactDOM.render(
+  <BrowserRouter>
     <Provider store={store}>
       <App />
-      </Provider>
-    </BrowserRouter>,
-    document.getElementById("root")
-  );
-};
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -12,25 +12,32 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_POST:
+    case ADD_POST: {
       let newPost = {
         id: 5,
         message: state.newPostText,
         likesCount: 14,
       };
-      state.posts.push(newPost);
-      state.newPostText = '';
-      return state;
-    case UPDATE_NEW_POST_TEXT:
-      state.newPostText = action.newText;
-      return state;
+      let stateCopy = { ...state };
+      stateCopy.posts = [...state.posts];
+      stateCopy.posts.push(newPost);
+      stateCopy.newPostText = "";
+      return stateCopy;
+    }
+    case UPDATE_NEW_POST_TEXT: {
+      let stateCopy = { ...state };
+      stateCopy.newPostText = action.newText;
+      return { ...state };
+    }
     default:
-        return state;
+      return state;
   }
 };
 
 export const addPostActionCreator = () => ({ type: ADD_POST });
-export const updateNewPostTextActionCreator = (text) => 
-  ({type: UPDATE_NEW_POST_TEXT,newText: text,});
+export const updateNewPostTextActionCreator = (text) => ({
+  type: UPDATE_NEW_POST_TEXT,
+  newText: text,
+});
 
 export default profileReducer;
