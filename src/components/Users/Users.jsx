c63b29a09746c52941e9d30c867668ac68edd5af
Maxwell6612 <Maxwell6612@protonmail.ch>
2021-07-04 22:26:55 +0300

    axios get

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './users.module.css'
 import axios from "axios";
+import userPhoto from '../../assets/images/zoom.jpg';
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
         
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers();
+            props.setUsers(response.data.items);
         });
 
 }
@@ -17,7 +18,7 @@ let Users = (props) => {
            props.users.map( u => <div key={u.id}>
            <span>
                <div>
-                   <img alt='' src={u.photoUrl} className={styles.userPhoto}/>
+                   <img alt='' src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                </div>
                <div>
                     { u.followed ? <button onClick={ () => { props.unfollow(u.id) }}>Unfollow</button> : <button onClick={ () => { props.follow(u.id) }}>Follow</button>}
@@ -25,11 +26,11 @@ let Users = (props) => {
            </span>
            <span>
                <span>
-                    <div>{u.fullname}</div>
+                    <div>{u.name}</div>
                     <div>{u.status}</div>
                </span>
                <span>
-                    <div>{u.location.country}</div><div>{u.location.city}</div>
+                    <div>{'u.location.country'}</div><div>{'u.location.city'}</div>
                </span>
            </span>
        </div>)}
